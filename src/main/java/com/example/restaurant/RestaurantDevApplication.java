5907ef5da9babf29873b82bcbfcb7cb13af2cac3
krimuru9336 <kritika.murugan@informatik.hs-fulda.de>
2022-11-27 20:10:53 +0100

    updated logo, pdf, made minor changes

--- src/main/java/com/example/restaurant/RestaurantDevApplication.java
+++ src/main/java/com/example/restaurant/RestaurantDevApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantDevApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
 		SpringApplication.run(RestaurantDevApplication.class, args);
 	}
 }
